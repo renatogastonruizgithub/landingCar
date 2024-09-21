02283268c8c373c952dbb438520031a2cf293f26
unknown <renatogastonruiz.00@gmail.com>
2024-09-21 20:14:37 -0300

    gallery

--- landing/src/components/gallery.jsx
+++ landing/src/components/gallery.jsx
@@ -1,124 +1,82 @@
-"use client"
-import Modal from "./modal"
-import { useState } from 'react';
+"use client";
+import Modal from "./modal";
+import { useState, useEffect } from "react";
 
-
- function Gallery() {
-  const [open, setOpen] = useState(false)
+function Gallery() {
+  const [open, setOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
+  const [gallery, setgallery] = useState([]);
+
+  useEffect(() => {
+    const fetchGallery = async () => {
+      try {
+        const response = await fetch(
+          "https://web-django-templates.vercel.app/api/gallery/",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        const data = await response.json();
+        setgallery(data); // Asigna los datos a 'services'
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching services:", error);
+      }
+    }; //fin fetchServices
+    fetchGallery();
+  }, []);
   const handleOpenModal = (imageSrc) => {
     setSelectedImage(imageSrc);
+    console.log(imageSrc);
     setOpen(true);
   };
 
   return (
     <div className="max-w-6xl mx-auto px-4 sm:px-6">
-      
-       <div className="py-12 md:py-20">
-
-          <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
-            <div className="grid gap-4">
-              <div className="group relative">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-55 lg:h-80">
-                    <img
-                      className="h-full w-full object-cover object-center lg:h-full lg:w-full rounded-lg object-cover object-center"
-                      src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80"
-                      alt="gallery-photo"
-                    />
-              
+      <div className="py-12 md:py-20">
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+          {gallery.map((gallery, g) => (
+            <>
+              <div className="grid gap-4">
+                <div key={g} className="group relative">
+                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200  group-hover:opacity-55 lg:h-80">
+                    {gallery.images.map((img) => (
+                      <>
+                        <img
+                          className="h-full w-full object-cover object-center lg:h-full lg:w-full rounded-lg object-cover object-center"
+                          src={img.url_image}
+                          alt={img.title}
+                        />
+                        <div className="flex items-star p-4">
+                          <button
+                            onClick={() =>
+                              handleOpenModal(`${img.url_image}`)
+                            }
+                            style={{fontSize:"24px",color:"white"}}
+                            type="button"
+                            className="relative z-10 w-full rounded-md  px-4 py-2 text-md  opacity-0 focus:opacity-100 group-hover:opacity-100"
+                          >
+                            Ver imagen
+                            
+                          </button>
+                        </div>
+                      </>
+                    ))}
                   </div>
-                  <div className="flex items-star p-4">
-                  <button onClick={() => handleOpenModal("https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1950&amp;q=80")}
-                  type="button" className="relative z-10 w-full rounded-md bg-white bg-opacity-75 px-4 py-2 text-sm text-gray-900 opacity-0 focus:opacity-100 group-hover:opacity-100" >Quick View<span cl="sr-only">, Basic Tee 6-Pack </span></button>
-                  </div>
-        
-                  
+                </div>
               </div>
-                
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg object-cover object-center "
-                  src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
-                  alt="gallery-photo"
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg object-cover object-center"
-                  src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
-                  alt="gallery-photo"
-                />
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg object-cover object-center"
-                  src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
-                  alt="gallery-photo"
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg object-cover object-center"
-                  src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
-                  alt="gallery-photo"
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg object-cover object-center "
-                  src="https://docs.material-tailwind.com/img/team-3.jpg"
-                  alt="gallery-photo"
-                />
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg object-cover object-center"
-                  src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2940&amp;q=80"
-                  alt="gallery-photo"
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg object-cover object-center "
-                  src="https://docs.material-tailwind.com/img/team-3.jpg"
-                  alt="gallery-photo"
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg object-cover object-center"
-                  src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
-                  alt="gallery-photo"
-                />
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg object-cover object-center"
-                  src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
-                  alt="gallery-photo"
-                />
-              </div>
-              <div>
-                <img
-                  className="h-auto max-w-full rounded-lg object-cover object-center"
-                  src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=927&amp;q=80"
-                  alt="gallery-photo"
-                />
-              </div>
-            </div>
-          </div>
+            </>
+          ))}
         </div>
+      </div>
 
-
-        {open && <Modal src={selectedImage} onClose={() => setOpen(false)} />}
+      {open && <Modal src={selectedImage} onClose={() => setOpen(false)} />}
     </div>
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
